4050e4fe5cc1ac662c2515182ee865d401e00b4e
charlysproule <charlysproule@gmail.com>
2021-02-15 13:52:04 +0000

    UI
    Added buttons and points

--- NIScreenGameJam/Assets/Scripts/Pickup.cs
+++ NIScreenGameJam/Assets/Scripts/Pickup.cs
@@ -5,42 +5,71 @@ using TMPro;
 
 namespace _Complete
 {
+<<<<<<< Updated upstream
     public class Pickup : MonoBehaviour
     {
         public Text countText;
         public Text countText2;
+=======
+    public class Pickup : MonoBehaviour { 
+
+        public Text countText;
+    
+>>>>>>> Stashed changes
         private int count;
 
         void Start()
         {
             count = 0;
             SetCountText ();
+<<<<<<< Updated upstream
             SetCountText2 ();
+=======
+
+>>>>>>> Stashed changes
         }
          void OnTriggerEnter2D(Collider2D other)
         {
             if (other.tag == "Letter")
             {
+<<<<<<< Updated upstream
                
                 //coinText.text = Count.ToString();
                 Destroy(other.gameObject);
                 count = count + 1;
                 SetCountText ();
 
+=======
+                count = count + 1;
+                //coinText.text = Count.ToString();
+                Destroy(other.gameObject);
+               
+                SetCountText ();
+>>>>>>> Stashed changes
             }
+
 
             if (other.tag == "Parcel")
             {
+<<<<<<< Updated upstream
                 Destroy(other.gameObject);
                 count = count + 3;
                 SetCountText2 ();
 
+=======
+
+                count = count + 3;
+                Destroy(other.gambeObject);
+
+                SetCountText2();
+>>>>>>> Stashed changes
 
             }
         }
 
         void SetCountText()
         {
+<<<<<<< Updated upstream
             countText.text = " " + count.ToString();
         }
 
@@ -50,4 +79,14 @@ namespace _Complete
         }
     }
 
+=======
+            countText.text = " " + count.ToString ();
+        }
+
+    }
+
+   
+>>>>>>> Stashed changes
 }
+
+
