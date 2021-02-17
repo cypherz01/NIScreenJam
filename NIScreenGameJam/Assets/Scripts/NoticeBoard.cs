480c47195c9caab872c3ec6bd6b11e987c170615
charlysproule <charlysproule@gmail.com>
2021-02-17 23:50:31 +0000

    Dialogue
    Dialogue Boxes

---
+++ NIScreenGameJam/Assets/Scripts/NoticeBoard.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoticeBoard : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+
+    public GameObject NoticeboardUI;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+
+            }
+            else
+            {
+                Pause();
+
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        NoticeboardUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        NoticeboardUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading menu...");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+    }
+}
