ae76b4d40e25a468b94feb3f2e9635d320db81f6
cypherz01 <mrtomcuthbertson@gmail.com>
2021-02-18 14:36:55 +0000

    added death zone + dialgoue fixing

---
+++ NIScreenGameJam/Assets/deathScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathScript : MonoBehaviour
+{
+    GameObject respawn;
+    GameObject cam;
+    GameObject cam_respawn;
+    GameObject Panelmanager;
+    new AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        respawn = GameObject.Find("startpos_0");
+        cam = GameObject.Find("MainCam");
+        cam_respawn = GameObject.Find("CamPos_0");
+        Panelmanager = GameObject.Find("position_manager");
+        audio = GameObject.Find("AudioManager_Death").GetComponent<AudioSource>(); 
+        
+    }
+
+    // Update is called once per fram
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.GetComponent<Rigidbody2D>().position = respawn.transform.position;
+            cam.transform.position = cam_respawn.transform.position;
+            Panelmanager.GetComponent<PanelManager>().i = 0;
+            Panelmanager.GetComponent<PanelManager>().y = 0;
+            audio.Play();
+        }
+    }
+}
